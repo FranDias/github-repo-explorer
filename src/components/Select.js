7444bcc1f43785836bba88f4e591a53f23f2fc75
Francisco Dias <frandias@gmail.com>
2019-03-25 23:23:58 -0400

    refactor(functional components)

--- src/components/Select.js
+++ src/components/Select.js
@@ -1,37 +1,26 @@
 import React, { PureComponent } from "react";
 import PropTypes from "prop-types";
 
-class Select extends PureComponent {
-  static propTypes = {
-    name: PropTypes.string,
-    onChange: PropTypes.func.isRequired,
-    options: PropTypes.arrayOf(PropTypes.string).isRequired
-  };
+const Select = ({ name, onChange, options }) => (
+  <select {...{ onChange, name }}>
+    {options.map(choice => (
+      <option key={choice} value={choice}>
+        Sort by {choice}
+      </option>
+    ))}
+  </select>
+);
 
-  static defaultProps = {
-    name: "default",
-    onChange: () => {},
-    options: ["no options provied"]
-  };
+Select.propTypes = {
+  name: PropTypes.string,
+  onChange: PropTypes.func.isRequired,
+  options: PropTypes.arrayOf(PropTypes.string).isRequired
+};
 
-  getname() {
-    const { name } = this.props;
-    return name !== undefined ? name : "";
-  }
-
-  render() {
-    const { onChange, name, options } = this.props;
-
-    return (
-      <select {...{ onChange, name }}>
-        {options.map(choice => (
-          <option key={choice} value={choice}>
-            Sort by {choice}
-          </option>
-        ))}
-      </select>
-    );
-  }
-}
+Select.defaultProps = {
+  name: "default",
+  onChange: () => {},
+  options: ["no options provied"]
+};
 
 export default Select;
